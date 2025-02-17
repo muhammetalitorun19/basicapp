206bfc3ff0a3b75993bfdfe9253671253b7774f2
muhammetalitorun19 <muhammetalitorun19@gmail.com>
2025-02-17 23:47:28 +0300

    counter.js

---
+++ counter.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+function increase() {
+    count++;
+    updateCounter();
+}
+
+function decrease() {
+    count--;
+    updateCounter();
+}
+
+function reset() {
+    count = 0;
+    updateCounter();
+}
+
+function updateCounter() {
+    document.getElementById("counter").innerText = count;
+}
